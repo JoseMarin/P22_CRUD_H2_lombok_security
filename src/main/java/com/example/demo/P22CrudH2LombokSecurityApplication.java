2ea7a7bd51bf6105b3b6fea2933ffdc89a5197b6
Jose Marín <jmarinmalagon@gmail.com>
2023-05-15 01:43:32 +0200

    new project

---
+++ src/main/java/com/example/demo/P22CrudH2LombokSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P22CrudH2LombokSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P22CrudH2LombokSecurityApplication.class, args);
+	}
+
+}
